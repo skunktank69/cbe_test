0e9b607d38ce622fab8f8cbade3255d03667b6e4
skunktank69 <apurvsikka@gmail.com>
2026-05-03 19:07:20 +0530

    eee

--- extensions/cbe_1/tests/extension.test.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async search(api) {
-    const res = await api.search("test");
-    if (!Array.isArray(res)) {
-      throw new Error("search must return array");
-    }
-  }
-};
---
+++ extensions/mangadotnet/main.js
@@ -0,0 +1,9 @@
+// MangaDotNet Extension
+
+async function search(q) {
+    return Cbe.mapResults([]);
+}
+
+async function getDetails(id) {
+    return { id, title: "MangaDotNet" };
+}
